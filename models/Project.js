1ac7443f3b0a1cae32cf84f2110a86be3ec0392e
gunnarsikorski <gunnarsikorski@gmail.com>
2020-11-17 12:33:35 -0600

    Built project model

---
+++ models/Project.js
@@ -0,0 +1,27 @@
+const mongoose = require('../db/connection')
+
+// const taskSchema = require('./Task')
+
+const ProjectSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String, 
+            required: true
+        },
+        description: String,
+        dueDate: {
+            type: Date,
+            required: true
+        },
+        completed: false,
+        links: String,
+        //tasks: [taskSchema]
+    }, 
+    {
+        timestamp: true
+    }
+)
+
+const Project = mongoose.model('Project', ProjectSchema)
+
+module.exports = Project
