da20dff8ace036796cdc8b01127b53c00c007f5e
gunnarsikorski <gunnarsikorski@gmail.com>
2020-11-17 13:00:34 -0600

    Set up controller

--- controllers/projects.js
+++ controllers/projects.js
@@ -7,10 +7,53 @@ const Project = require('../models/Project')
 
 router.get('/', (req, res, next) => {
     Project.find({})
+    .then((projects) => {
+        res.json(projects)
+    })
+    .catch(next)
 })
 
 // POST
 
+router.post('/', (req, res, next) => {
+    const projectData = req.body
+    Project.create(projectData)
+    .then((project) => {
+        res.json(project)
+    })
+    .catch(next)
+})
+
 // PATCH
 
-// DELETE
+router.patch('/:id', (req, res, next) => {
+    const id = req.params.id
+    const projectData = req.body
+    Project.findOneAndUpdate(
+			{ _id: id },
+			{
+				title: req.body.title,
+				description: req.body.description,
+				dueDate: req.body.dueDate,
+                completed: req.body.completed,
+                links: req.body.links,
+                //tasks: req.body.tasks
+			},
+			{ new: true }
+		)
+			.then((project) => res.json(project))
+			.catch(next);
+})
+
+// DESTROY
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id
+    Project.findByIdAndRemove(id)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+//____________________________________________________________
+
+module.exports = router
