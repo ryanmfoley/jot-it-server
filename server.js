3ad9a1e4a3462427d1068957d6098f0563287473
ryfoa6 <ryanmfoley84@gmail.com>
2021-07-09 13:41:03 -0700

    Rename index.js to server.js

--- server.js
+++ server.js
@@ -35,13 +35,8 @@ app.use('/api/tasks', taskController)
 const onConnect = (socket) => {
 	// Listen for name and room sent by client through the 'join' event
 	socket.on('joinRoom', async ({ name, room }) => {
-		// const newName = name.trim().toLowerCase()
-		// const newRoom = room.trim().toLowerCase()
-
-		// if (name && room) {
 		// Create User
 		let user = await new User(socket.id, name, room)
-		console.log('user', user)
 
 		// Add user to list or users
 		addUser(user)
@@ -61,7 +56,6 @@ const onConnect = (socket) => {
 		})
 
 		const users = await getUsersInRoom(user.room)
-		console.log('users', users)
 
 		// Send room info to the channel that the client is in
 		io.to(user.room).emit('usersInRoom', { users })
@@ -71,7 +65,7 @@ const onConnect = (socket) => {
 	// Listen for messages from client
 	socket.on('send-chat-message', async (message, clearMessage) => {
 		const user = await getCurrentUser(socket.id)
-		// console.log(user)
+
 		if (user) {
 			// Send messages to current users room
 			io.to(user.room).emit('chat-message', {
@@ -106,6 +100,6 @@ io.on('connection', onConnect)
 
 //______________________________________________________________
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
